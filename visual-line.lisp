1f719236d206737dc2cb5b008549b910676bf168
April & May <apr3vau@outlook.com>
2025-04-06 10:09:18 +0200

    Fix the dead lock

--- visual-line.lisp
+++ visual-line.lisp
@@ -16,79 +16,80 @@
 
 (in-package editor)
 
+;; `call-with-window-and-buffer-locked` is the only window locking
+;; method that not shaken in LW 8.0.1
 (defcommand "Beginning of Visual Line" (p)
      "Same with \"Beginning of Line\", but works with visual line."
      "Same with \"Beginning of Line\", but works with visual line."
-  (let* ((point  (current-point))
-         (window (current-window))
-         (y      (second (multiple-value-list
-                          (point-to-cursorpos point window)))))
+  (let ((window (current-window)))
     (call-with-window-and-buffer-locked
      window 2
      (lambda (&rest args)
        (declare (ignore args))
-       (unless (cursorpos-to-point 0 (if p (+ y p) y) window point)
-         (editor-error "No such line."))))))
+       (let* ((point (current-point))
+              (y     (nth-value 1 (point-to-cursorpos point window))))
+         (unless (cursorpos-to-point 0 (if p (+ y p) y) window point)
+           (editor-error "No such line.")))))))
 
 (defcommand "End of Visual Line" (p)
      "Same with \"End of Line\", but works with visual line."
      "Same with \"End of Line\", but works with visual line."
-  (let* ((point  (current-point))
-         (window (current-window))
-         (px     (window-width window))
-         (y      (second (multiple-value-list
-                          (point-to-cursorpos point window)))))
+  (let ((window (current-window)))
     (call-with-window-and-buffer-locked
      window 2
      (lambda (&rest args)
        (declare (ignore args))
-       (unless (cursorpos-to-point px (if p (+ y p) y) window point)
-         (editor-error "No such line."))))))
+       (let* ((point (current-point))
+              (px    (window-width window))
+              (y     (nth-value 1 (point-to-cursorpos point window))))
+         (unless (cursorpos-to-point px (if p (+ y p) y) window point)
+           (editor-error "No such line.")))))))
 
 (defcommand "Kill Visual Line" (p)
      "Same with \"Kill Line\", but works with visual line."
      "Same with \"Kill Line\", but works with visual line."
-  (let* ((buffer       (current-buffer))
-         (point        (buffer-point buffer))
-         (window       (current-window))
-         (px-end       (window-width window))
-         (y            (second (multiple-value-list
-                                (point-to-cursorpos point window))))
-         (error-string nil))
+  (let ((window (current-window)))
     (call-with-window-and-buffer-locked
      window 2
      (lambda (&rest args)
        (declare (ignore args))
-       (with-point ((tpoint point))
-         (cond
-          (p
-           (when (and (not (start-line-p point)) (minusp p))
-             (incf p))
-           (unless (cursorpos-to-point px-end (+ y p) window tpoint)
+       (let* ((buffer (current-buffer))
+              (point  (buffer-point buffer))
+              (px-end (window-width window))
+              (y      (nth-value 1 (point-to-cursorpos point window))))
+         (with-point ((tpoint point))
+           (cond
+            (p
+             (when (and (not (start-line-p point)) (minusp p))
+               (incf p))
+             (unless (cursorpos-to-point px-end (+ y p) window tpoint)
+               (if (plusp p)
+                 (%kill-region  point (buffer-%end buffer)
+                                :kill-forward)
+                 (%kill-region  (buffer-%start buffer) point 
+                                :kill-backward))
+               (editor-error))
              (if (plusp p)
-               (%kill-region  point (buffer-%end buffer)
-                              :kill-forward)
-               (%kill-region  (buffer-%start buffer) point 
-                              :kill-backward))
-             (editor-error))
-           (if (plusp p)
-             (%kill-region  point tpoint :kill-forward)
-             (%kill-region  tpoint point :kill-backward)))
-          (t
-           (unless (and (blank-after-p tpoint)
-                        (line-offset tpoint 1 0))
-             (cursorpos-to-point px-end (1+ y) window tpoint))
-           (if (point= point tpoint)
-             (setq error-string "Kill Line attempted at end of buffer")
-             (%kill-region point tpoint :kill-forward)))))))
-    (when error-string (editor-error error-string))))
+               (%kill-region  point tpoint :kill-forward)
+               (%kill-region  tpoint point :kill-backward)))
+            (t
+             (unless (and (blank-after-p tpoint)
+                          (line-offset tpoint 1 0))
+               (cursorpos-to-point px-end (1+ y) window tpoint))
+             (if (point= point tpoint)
+               (editor-error "Kill Line attempted at end of buffer")
+               (%kill-region point tpoint :kill-forward))))))))))
 
-(defvar *target-x-position* 0)
+(define-editor-variable visual-line-target-x-position 0
+   "Buffer-local variable for memorizing horizontal cursor position
+for line movement in Visual Line mode.")
 
 (defun set-target-x-position (point)
-  (if (eq (last-command-type) :line-motion)
-      *target-x-position*
-      (setq *target-x-position* (point-to-xy-pixels (current-window) point))))
+  (symbol-macrolet ((var (buffer-value (point-buffer point)
+                                       'visual-line-target-x-position)))
+    (if (eq (last-command-type) :line-motion)
+      var
+      (setf var (point-to-xy-pixels (current-window) point)))))
 
 (defcommand "Previous Visual Line" (p)
      "Same with \"Previous Line\", but works with visual line."
@@ -96,19 +97,20 @@
   (let* ((point  (current-point))
          (window (current-window))
          (height (get-window-height window))
-         (px     (set-target-x-position point))
-         (y      (second (multiple-value-list (point-to-cursorpos point window))))
+         (y      (nth-value 1 (point-to-cursorpos point window)))
          (offset (or p -1)))
     (when (or (< (+ y offset) 0)
               (> (+ y offset) height))
       (refresh-screen-command nil)
-      (setq y (second (multiple-value-list (point-to-cursorpos point window)))))
+      (setq y (nth-value 1 (point-to-cursorpos point window))))
     (call-with-window-and-buffer-locked
      window 2
      (lambda (&rest args)
        (declare (ignore args))
-       (unless (cursorpos-to-point px (if p (- y p) (1- y)) window point)
-         (editor-error "No previous line."))))
+       (let ((px (set-target-x-position point))
+             (y  (nth-value 1 (point-to-cursorpos point window))))
+         (unless (cursorpos-to-point px (if p (- y p) (1- y)) window point)
+           (editor-error "No previous line.")))))
     (setf (last-command-type) :line-motion)))
 
 (defcommand "Next Visual Line" (p)
@@ -117,27 +119,28 @@
   (let* ((point  (current-point))
          (window (current-window))
          (height (get-window-height window))
-         (px     (set-target-x-position point))
-         (y      (second (multiple-value-list (point-to-cursorpos point window))))
+         (y      (nth-value 1 (point-to-cursorpos point window)))
          (offset (or p -1)))
     (when (or (< (+ y offset) 0)
               (> (+ y offset) height))
       (refresh-screen-command nil)
-      (setq y (second (multiple-value-list (point-to-cursorpos point window)))))
+      (setq y (nth-value 1 (point-to-cursorpos point window))))
     (call-with-window-and-buffer-locked
      window 2
      (lambda (&rest args)
        (declare (ignore args))
-       (unless (cursorpos-to-point px (if p (+ y p) (1+ y)) window point)
-         (editor-error "No previous line."))))
+       (let ((px (set-target-x-position point))
+             (y  (nth-value 1 (point-to-cursorpos point window))))
+         (unless (cursorpos-to-point px (if p (+ y p) (1+ y)) window point)
+           (editor-error "No previous line.")))))
     (setf (last-command-type) :line-motion)))
 
 (defmode "Visual Line"
   :key-bindings '(("Beginning of Visual Line" "Control-a")
-                  ("End of Visual Line" "Control-e")
-                  ("Kill Visual Line" "Control-k")
-                  ("Previous Visual Line" "Control-p")
-                  ("Next Visual Line" "Control-n")))
+                  ("End of Visual Line"       "Control-e")
+                  ("Kill Visual Line"         "Control-k")
+                  ("Previous Visual Line"     "Control-p")
+                  ("Next Visual Line"         "Control-n")))
 
 (defcommand "Visual Line Mode" (p)
      "Toggle the Visual Line Mode. If P is supplied, turn on the mode
