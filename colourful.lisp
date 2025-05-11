17016fcf5a3f5d84b59cd56bdd27cd9faf758d0c
April & May <apr3vau@outlook.com>
2025-05-11 23:37:10 +0800

    New highlight scheme!

--- colourful.lisp
+++ colourful.lisp
@@ -44,15 +44,18 @@
            :italic-p t
            :foreground (editor::create-dark-background-switchable-color :orchid :pink)
            :if-exists :overwrite)
-(make-face 'variable-face
+(make-face 'dynamic-variable-face
            :foreground (editor::create-dark-background-switchable-color :darkgoldenrod :lightgoldenrod)
+           :if-exists :overwrite)
+(make-face 'variable-name-face
+           :foreground (editor::create-dark-background-switchable-color :aquamarine4 :aquamarine2)
            :if-exists :overwrite)
 
 (export '(declaration-face builtin-face type-face function-face macro-face special-operator-face variable-face))
 
 ;; Symbol categories
 
-(defvar *special-operators*
+(defvar *special-operators* 
   (loop for sym being each present-symbol of (find-package "CL")
         when (special-operator-p sym) collect sym))
 
@@ -75,6 +78,10 @@
   "Loop keywords from https://lispcookbook.github.io/cl-cookbook/iteration.html#appendix-list-of-loop-keywords")
 
 ;; Utils
+
+(defun apply-highlight (start end face)
+  (declare (inline apply-highlight))
+  (editor::font-lock-apply-highlight start end face))
 
 (defun collect-forms (point)
   (with-point ((start point)
@@ -122,6 +129,12 @@
                      t))
         while (editor::point-after point)))
 
+(defun skip-prefix (start)
+  (loop for attr = (character-attribute :lisp-syntax (character-at start 0))
+        while (eq attr :prefix)
+        do (editor::point-after start))
+  start)
+
 ;; Fontifying
 
 (defun fontify-symbol (start end)
@@ -153,7 +166,7 @@
                      ((fboundp sym)
                       'function-face)
                      ((boundp sym)
-                      'variable-face)
+                      'dynamic-variable-face)
                      (t nil))
                (cond ((string= (car split) "")
                       'builtin-face)
@@ -164,9 +177,9 @@
           (with-point ((p start))
             (character-offset p (+ (length (car split))
                                    (1- (length split))))
-            (editor::font-lock-apply-highlight start p 'type-face)
-            (when face (editor::font-lock-apply-highlight p end face)))
-          (when face (editor::font-lock-apply-highlight start end face)))))))
+            (apply-highlight start p 'type-face)
+            (when face (apply-highlight p end face)))
+          (when face (apply-highlight start end face)))))))
 
 (defun fontify-single-form (start end)
   "Fontify a single form, can be a symbol or a list, with prefix
@@ -187,28 +200,28 @@ fontify-symbol."
                     finally (return attr)))
       (case attr
         (:open-paren
-         (editor::font-lock-apply-highlight start point 'special-operator-face)
+         (apply-highlight start point 'special-operator-face)
          (fontify-list point end))
         (:constituent
-         (editor::font-lock-apply-highlight start point 'special-operator-face)
+         (apply-highlight start point 'special-operator-face)
          (fontify-symbol point end))))))
 
 (defun fontify-loop (lst)
   "Highlight loop keywords"
   (let ((1st (pop lst)))
-    (editor::font-lock-apply-highlight (first 1st) (second 1st) 'macro-face))
+    (apply-highlight (first 1st) (second 1st) 'macro-face))
   (dolist (form lst)
     (let* ((start (first form))
            (end (second form))
            (str (points-to-string start end)))
       (if (member str *loop-keywords-names* :test #'string-equal)
-        (editor::font-lock-apply-highlight start end 'builtin-face)
+        (apply-highlight start end 'builtin-face)
         (fontify-single-form start end)))))
 
 (defun fontify-declaration-list (lst)
   ;; FIXME: buggy?
   (let ((1st (pop lst)))
-    (editor::font-lock-apply-highlight (first 1st) (second 1st) 'declaration-face))
+    (apply-highlight (first 1st) (second 1st) 'declaration-face))
   (dolist (form lst)
     (let* ((start (first form))
            (attr (loop for c = (character-at start 0)
@@ -221,6 +234,86 @@ fontify-symbol."
           (form-offset start 1 t -1)
           (let ((children (collect-forms start)))
             (when children (fontify-declaration-list children))))))))
+
+(defun fontify-let (lst)
+  "Highlight `let' form, or something behave like `let'."
+  (let (1st)
+    (destructuring-bind (start end) (pop lst)
+      (fontify-single-form start end)
+      (setq 1st (string-left-trim '(#\# #\' #\@ #\, #\`) (points-to-string start end))))
+    (when lst
+      (destructuring-bind (start end) (pop lst)
+        (with-point ((prefix-end start))
+          (skip-prefix prefix-end)
+          (apply-highlight start prefix-end 'special-operator-face)
+          (if (eql (character-at prefix-end 0) #\()
+	    (when-let (sub (form-offset prefix-end 1 t -1))
+	      (when-let (children (collect-forms sub))
+		(if (or (member 1st '("dolist" "cl-dolist" "dotimes" "cl-dotimes"
+                                      "do-symbols" "do-all-symbols")
+                                :test #'string-equal)
+			(and (member 1st '("when-let" "when-let*" "if-let" "if-let*")
+                                     :test #'string-equal)
+                             (with-point ((pt (caar children)))
+                               (skip-prefix pt)
+                               (not (eql (character-at pt 0) #\( )))))
+                  (destructuring-bind (start end) (car children)
+                    (apply-highlight start end 'variable-name-face)
+                    (dolist (c (cdr children))
+                      (apply #'fontify-single-form c)))
+		  (dolist (child children)
+		    (destructuring-bind (start end) child
+                      (with-point ((prefix-end start))
+                        (skip-prefix prefix-end)
+                        (apply-highlight start prefix-end 'special-operator-face)
+                        (if (eql (character-at prefix-end 0) #\()
+                          (when-let (sub (form-offset prefix-end 1 t -1))
+                            (when-let (children (collect-forms sub))
+                              (destructuring-bind (start end)
+                                  (car children)
+                                (apply-highlight start end 'variable-name-face))
+                              (dolist (c (cdr children))
+                                (apply #'fontify-single-form c))))
+                          (apply-highlight prefix-end end 'variable-name-face))))))))
+            (fontify-symbol prefix-end end)))
+	))
+    (dolist (l lst)
+      (apply #'fontify-single-form l))))
+
+;; function arglist
+(defun fontify-ordinary-lambda-list-at-1 (lst)
+  "Highlight ordinary lambda list which is at the second of
+expresion (typically lambda expression)."
+  (destructuring-bind (start end) (pop lst)
+    (fontify-single-form start end))
+  (when lst
+    (destructuring-bind (start end) (pop lst)
+      (with-point ((prefix-end start))
+        (skip-prefix prefix-end)
+        (apply-highlight start prefix-end 'special-operator-face)
+        (if (eql (character-at prefix-end 0) #\()
+	  (when-let (sub (form-offset prefix-end 1 t -1))
+	    (let ((children (collect-forms sub)))
+	      (dolist (child children)
+		(destructuring-bind (start end) child
+                  (with-point ((prefix-end start))
+                    (skip-prefix prefix-end)
+                    (apply-highlight start prefix-end 'special-operator-face)
+                    (case (character-at prefix-end 0)
+                      (#\( (fontify-list prefix-end))
+                      (#\& (apply-highlight start end 'special-operator-face))
+                      (t (apply-highlight start end 'variable-name-face))))
+		  ))))
+          (fontify-symbol prefix-end end)))))
+  (dolist (l lst)
+    (apply 'fontify-single-form l)))
+
+(defun fontify-ordinary-lambda-list-at-2 (lst)
+  "Highlight ordinary lambda list which is at the third of
+expresion (typically defun)."
+  (destructuring-bind (start end) (pop lst)
+    (fontify-single-form start end))
+  (when lst (fontify-ordinary-lambda-list-at-1 lst)))
 
 (defun fontify-list (start &optional end)
   "Parse items inside the list, sends them to corresponding fontify
@@ -237,6 +330,23 @@ functions."
              (fontify-declaration-list forms))
             ((string-equal 1st "loop")
              (fontify-loop forms))
+            ((member 1st '("let" "let*" "when-let" "when-let*" "if-let" "if-let*"
+                           "alexandria:when-let" "alexandria:when-let*" "alexandria:if-let" "alexandria:if-let*"
+                           "prog" "prog*" "with-point" "editor:with-point"
+                           "dolist" "cl-dolist" "dotimes" "cl-dotimes" "seq-doseq"
+                           "do-symbols" "do-all-symbols" "cl-do-symbols" "cl-do-all-symbols"
+                           "with-slots" "with-accessors")
+                     :test #'string-equal)
+             (fontify-let forms))
+            ((member 1st '("lambda"
+                              "multiple-value-bind" "cl-multiple-value-bind"
+                              "destructuring-bind" "cl-destructuring-bind"
+                              "with-gensyms" "with-unique-names")
+                        :test #'string-equal)
+             (fontify-ordinary-lambda-list-at-1 forms))
+            ((member 1st '("defun" "defmacro" "defsubst" "defalias")
+			:test #'string-equal)
+             (fontify-ordinary-lambda-list-at-2 forms))
             (t (dolist (l forms) (apply #'fontify-single-form l)))))))
 
 ;; Main Function
